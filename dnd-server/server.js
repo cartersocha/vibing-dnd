ece205466ad32d20b8dcb3590103b545eb8d718c
cartersocha <cartersocha@gmail.com>
2025-10-14 23:57:08 -0700

    update port

--- dnd-server/server.js
+++ dnd-server/server.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
@@ -303,5 +303,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Backend running at http://localhost:${PORT}`)
+  console.log(`✅ Backend running on port ${PORT}`);
 });
