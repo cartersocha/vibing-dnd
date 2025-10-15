6ad4e955bcc45dbfe807b86e2b0f982d5f9b171c
cartersocha <cartersocha@gmail.com>
2025-10-15 15:56:12 -0700

    push

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,6 +1,6 @@
 // Catch-all API route for Vercel to forward /api/* requests to the main server handler.
 // This file ensures Vercel invokes the node function rather than serving static files.
 
-const handler = require('../server.js');
+const handler = require('../dnd-server/server.js');
 
 module.exports = handler;
